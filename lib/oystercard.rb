6eea10791e2d411b1f603344b0283e871dac8ad0
RichardWatkins1 <richardwatkins1@gmail.com>
2016-04-18 18:04:56 +0100

    up to challenge 8 complete

---
+++ lib/oystercard.rb
@@ -0,0 +1,32 @@
+
+class Oystercard
+
+	attr_reader :balance
+
+	MAXIMUM_BALANCE = 90
+
+	def initialize
+		@balance = 0
+	end
+
+	def top_up(amount)
+    	fail "Maximum balance #{MAXIMUM_BALANCE}exceeded" if amount + balance > MAXIMUM_BALANCE
+    	@balance += amount
+  	end
+
+  	def deduct(amount)
+  		@balance -= amount
+  	end
+
+  	def touch_in
+  		@in_use = true
+  	end
+
+  	def touch_out
+  		@in_use = false
+  	end
+
+  	def in_journey?
+  		@in_use
+  	end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,51 @@
+require 'oystercard'
+
 describe Oystercard do
+	subject(:oystercard) {described_class.new}
+	it 'I want an oystercard with a balance of 0' do
+		expect(oystercard.balance).to eq(0)
+	end
+
+	describe '#top up balance' do
+
+		it 'response to top up with 1 argument' do
+			expect(oystercard).to respond_to(:top_up).with(1).argument
+		end
+
+		it 'adds money to the balance on oystercard'do 
+			expect{oystercard.top_up 1}.to change{oystercard.balance}.by 1
+		end
+
+		it 'raises an expection if the balance goes above £90' do 
+			MAXIMUM_BALANCE = Oystercard::MAXIMUM_BALANCE
+    		subject.top_up(MAXIMUM_BALANCE)
+    		expect{ subject.top_up 1 }.to raise_error "Maximum balance #{MAXIMUM_BALANCE}exceeded"
+		end
+	end
+
+	describe '#deduct balance' do
+		it 'deducts an amount from balance on oystercard' do 
+			oystercard.top_up(20)
+			expect{oystercard.deduct 3}.to change{oystercard.balance}.by -3
+		end
+	end
+
+	describe '#touch in/out support' do
+		it "can touch in" do
+			oystercard.touch_in
+  			expect(oystercard).to be_in_journey
+		end
+
+		it "can touch out" do
+  			oystercard.touch_in
+  			oystercard.touch_out
+  			expect(oystercard).not_to be_in_journey
+		end
+
+		it 'is initially not in a journey' do
+  			expect(oystercard).not_to be_in_journey
+		end
+
+
+	end
 end
