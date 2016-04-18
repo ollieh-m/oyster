72165813bb43fa4d7ef20ad6e45a49c494117208
Max Provin <maxprovin@outlook.com>
2016-04-18 21:03:04 +0100

    step 6 maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,16 @@
 class Oystercard
 
+MAXIMUM_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
 
-  def top_up(money) 
-  	@balance += (money)
-  	return balance
-  end	
+  def top_up(money)
+    fail "cannot top up beyond £#{MAXIMUM_BALANCE} maximum" if (@balance + money) > MAXIMUM_BALANCE
+    @balance += money
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,6 +12,13 @@ describe Oystercard do
 
   it 'tops up the balance' do
   	expect{oystercard.top_up(20)}.to change{oystercard.balance}.by 20
-  end	
+  end
+
+it 'has a maximum limit of £90' do
+    # oystercard.top_up(90)
+    oystercard.top_up(Oystercard::MAXIMUM_BALANCE)
+    expect { oystercard.top_up(1) }.to raise_error "cannot top up beyond £#{Oystercard::MAXIMUM_BALANCE} maximum"
+  end
+
 
 end
