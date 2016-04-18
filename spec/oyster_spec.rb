254b30a1e802e65fa09d03d094f64cbca04593db
Amy Nicholson <missamynicholson@gmail.com>
2016-04-18 16:36:00 +0100

    Added oyster.rb file

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,10 @@
+require 'oyster'
+
 describe Oyster do
-	
+  subject(:oyster) {described_class.new}
+
+  it 'has a balance of 0 when initialized' do
+    expect(oyster.balance).to eq 0
+  end
+
 end
