b5c78549627fc45b6bbc94a455665eebf8af89a2
ollieh-m <ollie.hm@cantab.net>
2016-04-18 16:59:18 +0100

    Added maximum limit to oyster balance

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -2,8 +2,15 @@ class Oyster
 
   attr_reader :balance
 
+  LIMIT = 90
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+  	raise "Balance cannot exceed #{LIMIT}!" if balance + amount > LIMIT
+  	@balance += amount
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -7,4 +7,15 @@ describe Oyster do
     expect(oyster.balance).to eq 0
   end
 
+  context '#top_up' do
+  	it 'should increase the balance by the amount passed' do
+  		oyster.top_up(20)
+  		expect(oyster.balance).to eq 20
+  	end
+    it 'should raise an error if trying to exceed limit' do
+      oyster.top_up(Oyster::LIMIT)
+      expect{oyster.top_up(1)}.to raise_error "Balance cannot exceed #{Oyster::LIMIT}!"
+    end
+  end	
+
 end
