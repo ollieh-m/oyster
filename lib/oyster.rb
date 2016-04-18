cf3b98c48c3044dab16c37a71ec90472d4451bfb
Amy Nicholson <missamynicholson@gmail.com>
2016-04-18 18:00:32 +0100

    Charge on touch_out

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -3,7 +3,7 @@ class Oyster
   attr_reader :balance
 
   LIMIT = 90
-  MIN_BALANCE = 1
+  MIN_FARE = 1
 
   def initialize
     @balance = 0
@@ -14,21 +14,24 @@ class Oyster
   	@balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def touch_in
-    raise "Balance must be at least £1" if balance < MIN_BALANCE
+    raise "Balance must be at least £1" if balance < MIN_FARE
     @in_use = true
   end
 
   def touch_out
+    deduct(MIN_FARE)
     @in_use = false
   end
 
   def in_journey?
     @in_use
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,6 +2,13 @@ require 'oyster'
 
 describe Oyster do
   subject(:oyster) {described_class.new}
+  let(:oyster_topped_up) do
+    oyster = Oyster.new
+    oyster.top_up(min_fare)
+    oyster.touch_in
+    oyster
+  end
+  let(:min_fare) {Oyster::MIN_FARE}
 
   it 'has a balance of 0 when initialized' do
     expect(oyster.balance).to eq 0
@@ -18,34 +25,27 @@ describe Oyster do
     end
   end
 
-  context '#deduct' do
-    it 'should decrease the balance by the amount passed' do
-      oyster.deduct(20)
-      expect(oyster.balance).to eq -20
-    end
-  end
-
   context 'touching in and out' do
     it 'is initially not in a journey' do
       expect(oyster).not_to be_in_journey
     end
 
     it 'expects in_journey? to return true having touched in with balance at least 1' do
-      oyster.top_up(Oyster::MIN_BALANCE)
-      oyster.touch_in
-      expect(oyster).to be_in_journey
+      expect(oyster_topped_up).to be_in_journey
     end
 
     it 'expects in_journey? to return false having touched out' do
-      oyster.top_up(Oyster::MIN_BALANCE)
-      oyster.touch_in
-      oyster.touch_out
-      expect(oyster).not_to be_in_journey
+      oyster_topped_up.touch_out
+      expect(oyster_topped_up).not_to be_in_journey
     end
 
     it 'should raise an error when touching in with balance less than 1' do
-      oyster.top_up(Oyster::MIN_BALANCE-1)
+      oyster.top_up(min_fare-1)
       expect{oyster.touch_in}.to raise_error "Balance must be at least £1"
+    end
+
+    it 'should decrease balance by MIN_FARE when touch out' do
+      expect {oyster_topped_up.touch_out}.to change{oyster_topped_up.balance}.by(-min_fare)
     end
   end
 
