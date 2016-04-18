add5d0f0587c555dde55cbf37a2ec3917a1051a8
Max Provin <maxprovin@outlook.com>
2016-04-18 22:27:21 +0100

    neatened up tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,23 +4,29 @@ describe Oystercard do
 
   subject(:oystercard){ described_class.new }
 
-  it { is_expected.to respond_to(:balance) }
-  it { is_expected.to respond_to(:deduct).with(1).argument }
+  context 'balance' do
 
+    it { is_expected.to respond_to(:balance) }
+    it { is_expected.to respond_to(:deduct).with(1).argument }
 
-  it 'has a default balance of 0 on initialization' do
-    expect(oystercard.balance).to eq 0
+    it 'has a default balance of 0 on initialization' do
+      expect(oystercard.balance).to eq 0
+    end
+
+    describe '#top_up' do
+      it 'tops up the balance' do
+  	    expect{oystercard.top_up(20)}.to change{oystercard.balance}.by +20
+      end
+    end
+
+    describe '#deduct' do
+      it 'deducts a value from balance' do
+        expect{oystercard.deduct(20)}.to change{oystercard.balance}.by -20
+      end
+    end
   end
 
-  it 'tops up the balance' do
-  	expect{oystercard.top_up(20)}.to change{oystercard.balance}.by +20
-  end
-
-  it 'deducts a value from balance' do
-    expect{oystercard.deduct(20)}.to change{oystercard.balance}.by -20  end
-
-it 'has a maximum limit of £90' do
-    # oystercard.top_up(90)
+  it 'has a maximum limit of £90' do
     oystercard.top_up(Oystercard::MAXIMUM_BALANCE)
     expect { oystercard.top_up(1) }.to raise_error "cannot top up beyond £#{Oystercard::MAXIMUM_BALANCE} maximum"
   end
