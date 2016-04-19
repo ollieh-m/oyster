5568e9a4db0faa08392593614a444fcd9459cca5
ollieh-m <ollie.hm@cantab.net>
2016-04-19 12:42:58 +0100

    Challenge 12 - create record of journeys

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,7 @@
 
 class Oystercard
 
-	attr_reader :balance, :entry_station
+	attr_reader :balance, :entry_station, :exit_station, :journeys
 
 	MAXIMUM_BALANCE = 90
 
@@ -9,6 +9,7 @@ class Oystercard
 
 	def initialize
 		@balance = 0
+    @journeys = []
 	end
 
 	def top_up(amount)
@@ -22,9 +23,11 @@ class Oystercard
     @entry_station = station
   end
 
-  def touch_out
+  def touch_out(station)
   	@in_use = false
     deduct(MINIMUM_FARE)
+    @exit_station = station
+    @journeys[0] = { entry_station: @entry_station, exit_station: @exit_station }
     @entry_station = nil
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,10 +3,12 @@ require 'oystercard'
 describe Oystercard do
 
 	subject(:oystercard) {described_class.new}
-	let(:station) {double :station}
+	let(:entry_station) {double :station}
+	let(:exit_station) {double :station}
+
 	let(:oystercard_topped) do
 		subject.top_up(Oystercard::MINIMUM_FARE)
-		subject.touch_in(station)
+		subject.touch_in(entry_station)
 		subject
 	end
 
@@ -36,7 +38,7 @@ describe Oystercard do
 		end
 
 		it 'can touch out' do
-  			oystercard_topped.touch_out
+  			oystercard_topped.touch_out(exit_station)
   			expect(oystercard_topped).not_to be_in_journey
 		end
 
@@ -45,24 +47,38 @@ describe Oystercard do
 		end
 
 		it 'fails to touch in if balance is below the minimum' do 
-			expect{oystercard.touch_in(station)}.to raise_error "balance must be at least £#{Oystercard::MINIMUM_FARE}"
+			expect{oystercard.touch_in(entry_station)}.to raise_error "balance must be at least £#{Oystercard::MINIMUM_FARE}"
 		end
 
 		it 'deducts minimum fare when touching out' do
-			expect{oystercard_topped.touch_out}.to change{oystercard_topped.balance}.by -Oystercard::MINIMUM_FARE
+			expect{oystercard_topped.touch_out(exit_station)}.to change{oystercard_topped.balance}.by -Oystercard::MINIMUM_FARE
 		end
 	end
 
-	context 'storing journy information' do 
-		it 'it records the station you check in at' do 
-			expect(oystercard_topped.entry_station).to eq station
-		end
+	context 'storing journey information' do 
+		# it 'records the station you touch in at' do 
+		# 	expect(oystercard_topped.entry_station).to eq entry_station
+		# end
 
-		it 'it resets the entry_station on touch out' do
-			oystercard_topped.touch_out
+		it 'resets the entry_station on touch out' do
+			oystercard_topped.touch_out(exit_station)
 			expect(oystercard_topped.entry_station).to eq nil
 		end
 
+		# it 'records the station you touch out at' do
+		# 	oystercard_topped.touch_out(exit_station)
+		# 	expect(oystercard_topped.exit_station).to eq exit_station
+		# end
+
+		it 'has no journeys stored when card is created' do
+			expect(oystercard.journeys).to be_empty
+		end
+
+		it 'creates an entry in journeys with entry and exit stations stored in a hash' do
+			journey = { entry_station: entry_station, exit_station: exit_station }
+			oystercard_topped.touch_out(exit_station)
+			expect(oystercard_topped.journeys).to include journey
+		end
 	end
 
 end
