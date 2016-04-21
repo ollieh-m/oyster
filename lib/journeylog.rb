1b8653bd4a6745b1ad329ea81f7e0eb5d3396a9e
ollieh-m <ollie.hm@cantab.net>
2016-04-21 21:58:05 +0100

    Add current journey reader to journey log

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,6 +1,6 @@
 class Journeylog
 
-	attr_reader :journey_history
+	attr_reader :journey_history, :current_journey
 
 	def initialize(current_journey=Journey.new)
 		@current_journey = current_journey
@@ -8,18 +8,18 @@ class Journeylog
 	end
 
 	def begin(station,card)
-		if @current_journey.entry_station?
-			card.deduct(@current_journey)
-			@journey_history << {entrystation: @current_journey.start_station, exitstation: @current_journey.end_station}
+		if current_journey.entry_station?
+			card.deduct(current_journey)
+			@journey_history << {entrystation: current_journey.start_station, exitstation: current_journey.end_station}
 		end
-		@current_journey.start_journey(station)
+		current_journey.start_journey(station)
 	end
 
 	def finish(station,card)
-		@current_journey.end_journey(station)
+		current_journey.end_journey(station)
 		card.deduct(@current_journey)
-		@journey_history << {entrystation: @current_journey.start_station, exitstation: @current_journey.end_station}
-		@current_journey.reset
+		@journey_history << {entrystation: current_journey.start_station, exitstation: current_journey.end_station}
+		current_journey.reset
 	end
 
 end
